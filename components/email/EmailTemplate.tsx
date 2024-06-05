ccbe13717a72559f86ddd7a92118c418f7409c39
Frank Girse <fgirse@mac-mini-von-frank.home>
2024-06-05 23:09:00 +0200

    commit   2er

---
+++ components/email/EmailTemplate.tsx
@@ -0,0 +1,7 @@
+export default function EmailTemplate({ content = 'Hello world' }: {
+	content: string;
+  }) {
+	return <div>
+	  <div dangerouslySetInnerHTML={{ __html: content }}></div>
+	</div>
+  };
