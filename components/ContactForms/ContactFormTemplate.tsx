12c29af44afb8379b8c15d726a2b92f1ec99804e
Frank Girse <fgirse@mac-mini-von-frank.home>
2024-06-05 22:48:45 +0200

    commit initialer

--- components/ContactForms/ContactFormTemplate.tsx
+++
@@ -1,9 +0,0 @@
-type ContactFormTemplateProps = {
-  message: string;
-};
-
-export default function ContactFormTemplate({
-  message,
-}: ContactFormTemplateProps) {
-  return <div style={{ whiteSpace: 'pre-line' }}>{message}</div>;
-}
--- components/email/EmailTemplate.tsx
+++
@@ -1,7 +0,0 @@
-export default function EmailTemplate({ content = 'Sehr Frau / Herr ' }: {
-	content: string;
-  }) {
-	return <div>
-	  <div dangerouslySetInnerHTML={{ __html: content }}></div>
-	</div>
-  };
